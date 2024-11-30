c8727526f99c1c4b0f3292fb317ddebf3a1aa452
programmerjosco <programmerjosco@gmail.com>
2024-11-30 10:53:50 +0530

    addred

--- joscoonline/joscoonline_app/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from joscoonline_app.models import CustomUser
-# # Register your models here.
-admin.site.register(CustomUser)
--- joscoonline/joscoonline_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class JoscoonlineAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'joscoonline_app'
