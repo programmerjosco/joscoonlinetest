9c4fd5d4a9648ee521599b7c143934bd8e984f47
programmerjosco <programmerjosco@gmail.com>
2024-12-06 18:31:00 +0530

    added

--- joscoonline/joscoonline_app/models.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class CustomUser(AbstractUser):
-    branchid = models.IntegerField(blank=True, null=True, verbose_name="Branch ID")
-
-    def __str__(self):
-        return self.username
