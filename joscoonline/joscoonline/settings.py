d418eca26b9749a5aa292017b6f6d7282eba516d
programmerjosco <programmerjosco@gmail.com>
2024-12-08 14:48:50 +0530

    Resolved merge conflicts

--- joscoonline/joscoonline/settings.py
+++ joscoonline/joscoonline/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL='/media/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
